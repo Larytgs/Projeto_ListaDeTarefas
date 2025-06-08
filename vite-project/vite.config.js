f16685fed987fdd1ae3edc86a56dde6cf2895438
laryTgs <laryssatortato99@gmail.com>
2025-06-07 21:06:18 -0300

    up

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Projeto_ListaDeTarefas/', // Substitua pelo nome do seu repositório
-})
+  //base: '/Projeto_ListaDeTarefas/', // Substitua pelo nome do seu repositório
+});
