88e99805a979e7bc05c6fb1f964e04610c4e9828
laryTgs <laryssatortato99@gmail.com>
2025-06-20 15:48:11 -0300

    conectado ao MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 require("dotenv").config(); // Carrega as variáveis de ambiente do .env
-console.log(process.env.MONGODB_URI);
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors"); // Importa o pacote cors
